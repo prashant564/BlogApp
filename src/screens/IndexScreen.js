d9541cbb14c423e6e33d3e75fb3f7d8852e337a8
Dprash <prashantdixit600@gmail.com>
2021-01-05 21:01:44 +0530

    Add and remove blog done

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,46 @@
+import React, { useContext } from 'react';
+import { View, StyleSheet, Text, FlatList, Button, TouchableOpacity } from 'react-native';
+import { Context } from '../context/BlogContext';
+import { AntDesign } from '@expo/vector-icons'; 
+
+
+const IndexScreen = () => {
+    const { state, addBlogPost, deleteBlogPost } = useContext(Context);
+    return (
+        <View>
+            <Button title="Add Post" onPress={addBlogPost} />
+            <FlatList
+                data={state}
+                keyExtractor={(blogPost) => blogPost.title}
+                renderItem={({ item }) => {
+                    return <View style={styles.row}>
+                            <Text style={styles.title}>{item.title} - {item.id}</Text>
+                            <TouchableOpacity onPress={ () => deleteBlogPost(item.id) }>
+                                <AntDesign  name="delete" style={styles.icon}/>
+                            </TouchableOpacity>
+                            
+                            </View>
+                }}
+             />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
+});
+
+export default IndexScreen;
